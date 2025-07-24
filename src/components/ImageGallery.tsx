9c570a5b87bc3d0ae08b59147eb484f833744d19
Jonas Beck <dev@jonasbeck.dk>
2025-07-24 22:11:01 +0200

    chore: update styling
    Signed-off-by: Jonas Beck <dev@jonasbeck.dk>

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -99,7 +99,7 @@ export default function ImageGallery({
       </div>
 
       {/* Image Grid */}
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 md:gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-3 md:gap-4">
         {paginatedImages.map((image, index) => (
           <div
             key={startIndex + index}
